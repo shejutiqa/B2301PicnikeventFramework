1e89296bca37ead943cfcada04d2c8eb24a8be77
shejutiqa <meshejuti@gmail.com>
2023-07-24 20:10:01 -0500

    after adding class

---
+++ src/test/java/com/izaanschool/eventpic/ChildrenSelectTest.java
@@ -0,0 +1,13 @@
+package com.izaanschool.eventpic;
+
+import com.izaanschool.base.BaseTest;
+import com.izaanschool.page.PicnickEventPage;
+
+public class ChildrenSelect extends BaseTest {
+
+
+    public void NoChildren(){
+        PicnickEventPage cs=new PicnickEventPage(driver,log)
+    }
+
+}
